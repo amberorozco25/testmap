bc73e75b0ff67cb8e3dbad7f764e16dbb22ea616
amberorozco25 <103281391+amberorozco25@users.noreply.github.com>
2022-04-08 14:12:31 -0400

    Add files via upload

---
+++ data/amenity_bbq_4.js
@@ -0,0 +1,1 @@
+var json_amenity_bbq_4 = {"type":"FeatureCollection","name":"amenity_bbq_4","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"full_id":"n7902896308","osm_id":"7902896308","osm_type":"node","amenity":"bbq"},"geometry":{"type":"Point","coordinates":[-83.3648533,33.954041]}}]}
